ddce1396572fbbfa8cad5aee3b46b007bf5f3009
Julius Häger <julius_hager@hotmail.com>
2026-05-03 00:56:08 +0200

    Moved extensions file.

--- src/Generators/GeneratorBase/Utility/DocumentationExtensions.cs
+++ src/Generators/GeneratorBase/Utility/DocumentationExtensions.cs
@@ -5,7 +5,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
-namespace GeneratorBase
+namespace GeneratorBase.Utility
 {
     public static class DocumentationExtensions
     {
